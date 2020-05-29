4a37678a97a043b6f0ffc72f4aae6e07ff529e3a
Jason Tame <jason@pango.studio>
2020-05-29 20:32:23 +0200

    Remove unnecessary conditional

--- algorithms/math/fibonacci.js
+++ algorithms/math/fibonacci.js
@@ -1,14 +1,12 @@
 function fibonacci(index) {
-	if (index === 0 || index === 1) {
-		return 1
-	} else {
-		let sequence = [1, 1]
+	{
+		const sequence = [1, 1]
 		let next = 0
 		for (i = 2; i <= index; i++) {
 			next = sequence[i - 1] + sequence[i - 2]
 			sequence.push(next)
-		}
-
+        }
+        
 		return sequence[index]
 	}
 }
